b6431dcfae91aec1bcf3b34c409cb5b76c2c475e
Viktor Pastushenko <pastushenko.vik@gmail.com>
2020-06-04 11:28:22 +0300

    Updates

---
+++ precache-manifest.76e136da6902e12af5bfdcc3666251a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783e42ee95d544bf605e39a475d5ccff",
+    "url": "/memcrab/index.html"
+  },
+  {
+    "revision": "9a6e9d89545a747b047a",
+    "url": "/memcrab/static/css/main.e958df54.chunk.css"
+  },
+  {
+    "revision": "de0393a1c3c3abe100b2",
+    "url": "/memcrab/static/js/2.65e7d417.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memcrab/static/js/2.65e7d417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6e9d89545a747b047a",
+    "url": "/memcrab/static/js/main.5f4bcb91.chunk.js"
+  },
+  {
+    "revision": "c490e4867d9e845a4322",
+    "url": "/memcrab/static/js/runtime-main.fe5b52ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memcrab/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6cf04631d37e95e8f866aadd72f48af7.js"
+  "/memcrab/precache-manifest.76e136da6902e12af5bfdcc3666251a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/memcrab/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
