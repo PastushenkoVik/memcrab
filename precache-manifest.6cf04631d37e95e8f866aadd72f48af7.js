e643568577aeed54ee221ff5e2655fbb142cf02f
Viktor Pastushenko <pastushenko.vik@gmail.com>
2020-06-04 10:41:35 +0300

    Updates

---
+++ precache-manifest.6cf04631d37e95e8f866aadd72f48af7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd3ef4de36c03dac49889af4e4a2caf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ac4d7bda825ee68baa8",
+    "url": "/static/css/main.e958df54.chunk.css"
+  },
+  {
+    "revision": "de0393a1c3c3abe100b2",
+    "url": "/static/js/2.65e7d417.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.65e7d417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac4d7bda825ee68baa8",
+    "url": "/static/js/main.a4768b95.chunk.js"
+  },
+  {
+    "revision": "f866cd04515fe9e59514",
+    "url": "/static/js/runtime-main.8bcc4eee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
